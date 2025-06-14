54fd89b3159d09e3ea7fe3d946c1d89de729fa7f
rounakpal <rounakpal902@gmail.com>
2025-06-14 10:00:31 +0530

    Intial commit with login feature

---
+++ Page Object Model/loginPage.js
@@ -0,0 +1,30 @@
+class Login {
+   // Navigates to the Sauce Demo login page
+  visit() {
+    cy.visit("https://www.saucedemo.com/");
+  }
+// Enters the provided username into the username input field
+  setusername(username) {
+    cy.get("#user-name").type(username);
+  }
+  // Enters the provided password into the password input field
+  setpassword(password) {
+    cy.get("#password").type(password);
+  }
+ // Clicks the login button to attempt login
+    clickLogin() {
+        cy.get("#login-button").click();
+    }
+// Verifies that a login error message is visible
+    verifyLoginError() {
+        cy.get("[data-test='error']").should("be.visible");
+    }
+
+  // Verifies successful login by checking the URL and page title
+    verifyLoginSuccess() {
+        cy.url().should("include", "/inventory.html");
+        cy.get(".title").should("contain", "Products");
+    }
+}
+
+export default Login;
---
+++ cypress/e2e/Login.cy.js
@@ -0,0 +1,41 @@
+import Login from '../Page Object Model/Login';
+// Test suite for the Login Page functionality
+describe('Test Scenario for the Login Page', () => {
+  let loginData;
+
+// Load login data from fixture before all tests
+  before(() => {
+    cy.fixture("loginData").then((data) => {
+      loginData = data;
+    });
+  });
+  // Visit the login page before each test
+  beforeEach(() => {
+    Login.visit();
+  });
+  // Test: Successful login with valid credentials
+  it('Should login successfully with valid credentials', () => {
+    loginPage.setusername(loginData.validUser.username);
+    loginPage.setpassword(loginData.validUser.password);
+    loginPage.clickLogin();
+    loginPage.verifyLoginSuccess(); 
+  })
+// Test: Validation error with invalid credentials
+  it('Should give validation error with invalid credentials', () => {
+    loginPage.setusername(loginData.invalidUser.username);
+    loginPage.setpassword(loginData.invalidUser.password);
+    loginPage.clickLogin();
+    loginPage. verifyLoginError(); 
+  });
+// Test: Validation error for  empty username and password fields
+  it('Should give validation error for empty username and password fields', () => {
+    loginPage.setusername(''); 
+    loginPage.setpassword('');
+    loginPage.clickLogin();
+    loginPage.verifyLoginError();
+  });
+// Test: Validation error for empty password field and valid user name 
+ it('', () => {
+  
+ });
+})
