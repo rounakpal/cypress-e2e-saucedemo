5d716a3e7b41952289ca74d1e4ed2fff7ac9b6ea
rounakpal <rounakpal902@gmail.com>
2025-06-14 15:39:35 +0530

    assertion for login page

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,9 +1,9 @@
-import Login from '../Page Object Model/Login';
+import Login from "../Page Object Model/Login";
 // Test suite for the Login Page functionality
-describe('Test Scenario for the Login Page', () => {
+describe("Test Scenario for the Login Page", () => {
   let loginData;
 
-// Load login data from fixture before all tests
+  // Load login data from fixture before all tests
   before(() => {
     cy.fixture("loginData").then((data) => {
       loginData = data;
@@ -14,28 +14,30 @@ describe('Test Scenario for the Login Page', () => {
     Login.visit();
   });
   // Test: Successful login with valid credentials
-  it('Should login successfully with valid credentials', () => {
+  it("Should login successfully with valid credentials", () => {
     loginPage.setusername(loginData.validUser.username);
     loginPage.setpassword(loginData.validUser.password);
     loginPage.clickLogin();
-    loginPage.verifyLoginSuccess(); 
-  })
-// Test: Validation error with invalid credentials
-  it('Should give validation error with invalid credentials', () => {
+    loginPage.verifyLoginSuccess();
+  });
+  // Test: Validation error with invalid credentials
+  it("Should give validation error with invalid credentials", () => {
     loginPage.setusername(loginData.invalidUser.username);
     loginPage.setpassword(loginData.invalidUser.password);
     loginPage.clickLogin();
-    loginPage. verifyLoginError(); 
+    loginPage.verifyLoginError();
   });
-// Test: Validation error for  empty username and password fields
-  it('Should give validation error for empty username and password fields', () => {
-    loginPage.setusername(''); 
-    loginPage.setpassword('');
+  // Test: Validation error for  empty username and password fields
+  it("Should give validation error for empty username and password fields", () => {
+    loginPage.setusername("");
+    loginPage.setpassword("");
     loginPage.clickLogin();
     loginPage.verifyLoginError();
   });
-// Test: Validation error for empty password field and valid user name 
- it('', () => {
-  
- });
-})
+  // Test: Validation error for empty password field and valid user name
+  it("Should give validation error for the empty password field and valid user name", () => {
+     loginPage.setusername("Admin");
+    loginPage.clickLogin();
+    loginPage.verifyLoginError();
+  });
+});
