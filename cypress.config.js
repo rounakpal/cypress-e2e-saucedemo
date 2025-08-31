020f4fb59c68946f75e417f2fe88f95c769ff0eb
rounakpal <rounakpal902@gmail.com>
2025-08-31 14:15:54 +0530

    adding generate test report and parallel execution

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,12 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true
+    }
   },
 });
